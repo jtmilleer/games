12f587e1af0155108153bf43fbfbebaf5aaba0c3
jtmilleer <jtmilleer05@gmail.com>
2025-12-31 11:18:03 -0600

    distributeDeck  distributes the deck to 2 players

--- assets/src/decks/StandardDeck.java
+++ assets/src/decks/StandardDeck.java
@@ -3,29 +3,30 @@ package decks;
 import cards.PlayingCard;
 import cards.Rank;
 import cards.Suit;
+import players.AbstractPlayer;
 
 public class StandardDeck extends AbstractDeck<PlayingCard>{
 
     /**
      * Default constructor that initializes the capacity to 52.
+     * Initializes deck and shuffles.
      */
     public StandardDeck(){
         super(52);
-    }
-
-    /**
-     * Adds all standard cards to the deck, then shuffles them.
-     */
-    @Override
-    public void initalizeDeck() {
         for(Suit suit: Suit.values()){
             for(Rank rank: Rank.values()){
-                this.addCard(new PlayingCard(suit.name(),rank.name()));
+                this.addCard(new PlayingCard(suit,rank));
             }
         }
         this.shuffle();
     }
 
+    public <T extends AbstractPlayer> void distributeDeck(final T p1, final T p2, final int numCards){
+        for(int i = 0; i < numCards; i++){
+            p1.addCardToHand(this.draw());
+            p2.addCardToHand(this.draw());
+        }
+    }
 
 }
 
