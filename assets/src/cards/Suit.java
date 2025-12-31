60550b2dabec4da4597b8dec6351e0288ea709fc
jtmilleer <jtmilleer05@gmail.com>
2025-12-31 11:18:11 -0600

    small changes

--- assets/src/cards/Suit.java
+++ assets/src/cards/Suit.java
@@ -1,5 +1,13 @@
 package cards;
 
 public enum Suit {
-    CLUBS, DIAMONDS, HEARTS, SPADES;
+    CLUBS("Clubs"), DIAMONDS("Diamonds"), HEARTS("Hearts"), SPADES("Spades");
+
+    private final String suit;
+
+    private Suit(final String suit){
+        this.suit = suit;
+    }
+
+    public String getSuit(){return this.suit;}
 }
