33b277479a1022926ea285d3c559a4499c9306ac
jtmilleer <jtmilleer05@gmail.com>
2026-01-08 22:38:45 -0600

    moved enums into the class

--- assets/src/cards/PlayingCard.java
+++ assets/src/cards/PlayingCard.java
@@ -14,7 +14,7 @@ public class PlayingCard extends AbstractCard{
     private static int count;
 
     public PlayingCard(final Suit suit, final Rank rank) {
-        super(rank + " of " + suit, "PlayingCard_" + ++count);
+        super(rank + " of " + suit, "PlayingCard_" + ++count,suit,rank);
         this.suit = suit;
         this.rank = rank;
 
@@ -49,15 +49,92 @@ public class PlayingCard extends AbstractCard{
     public Suit getSuit(){return this.suit;}
     public Rank getRank(){return this.rank;}
 
+    /*
     @Override
     public String toString(){
-        return cardRep(this.getSuit().getSuit(),this.getRank().getRankString());
+        final String suitFormatted = this.getSuit().substring(0,1);
+        return cardRep(suitFormatted,this.rank.getCardValue());
     }
+     */
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof PlayingCard p)) return false;
         return rank == p.rank && suit == p.suit;
+    }
+
+
+    public enum Rank implements CardValue{
+        TWO(2),
+        THREE(3),
+        FOUR(4),
+        FIVE(5),
+        SIX(6),
+        SEVEN(7),
+        EIGHT(8),
+        NINE(9),
+        TEN(10),
+        JACK(11),
+        QUEEN(12),
+        KING(13),
+        ACE(14);
+
+        private final int value;
+
+        private final String rankString;
+
+        Rank(int value) {
+            if(value < 11){
+                this.rankString = Integer.toString(value);
+            }
+            else{
+                switch(value){
+                    case 11:{
+                        this.rankString = "J";
+                        break;
+                    }
+                    case 12:{
+                        this.rankString = "Q";
+                        break;
+                    }
+                    case 13:{
+                        this.rankString = "K";
+                        break;
+                    }
+                    case 14:{
+                        this.rankString = "A";
+                        break;
+                    }
+                    default:{
+                        throw new IllegalArgumentException("Rank not set");
+                    }
+                }
+            }
+
+            this.value = value;
+        }
+
+        /**
+         * PlayingCard's implementation of getCardValue() returns the Rank of the card.
+         * {[1,10],J,Q,K,A} are possible values.
+         * @return <code>String</code> of the card's rank.
+         */
+        public String getCardValue() {return rankString;}
+
+        public int getNumericValue(){return this.value;}
+
+    }
+
+    public enum Suit implements CardType{
+        CLUBS("Clubs"), DIAMONDS("Diamonds"), HEARTS("Hearts"), SPADES("Spades");
+
+        private final String suit;
+
+        private Suit(final String suit){
+            this.suit = suit;
+        }
+
+        public String getCardType(){return this.suit;}
     }
 }
