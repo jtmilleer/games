a2de7165ce09ac8acb9cd634cabce68f6cd24eee
jtmilleer <jtmilleer05@gmail.com>
2026-01-08 22:34:46 -0600

    Some javadocs and refactored toString for AbstractCard

--- assets/src/cards/AbstractCard.java
+++ assets/src/cards/AbstractCard.java
@@ -10,18 +10,58 @@ public abstract class AbstractCard extends GameObject{
 
     private static int count;
 
-    public AbstractCard(String name) {
+    //remove this constructor??
+    /**
+     * 3 param Constructor for <code>AbstractCard</code>
+     * @param name name for Card, gets passed into <code>GameObject</code> constructor
+     * @param type Some object that implements <code>CardType</code>, usually an enum
+     * @param value Some object that implements <code>CardValue</code>, usually an enum
+     */
+    public AbstractCard(String name, final CardType type, final CardValue value) {
         super(name,"AbstractCard_" + ++count);
+        this.cardType = type;
+        this.cardValue = value;
         this.name = name;
         faceUp = false;
     }
 
-    public AbstractCard(String name, String id){
+    /**
+     * 4 param Constructor for <code>AbstractCard</code>. This constructor should be the one used, as an
+     * id should always be specified, not using the AbstractCard id.
+     * @param name name for Card, gets passed into <code>GameObject</code> constructor
+     * @param id id for card, usually created in constructor of subclass
+     * @param type Some object that implements <code>CardType</code>, usually an enum
+     * @param value Some object that implements <code>CardValue</code>, usually an enum
+     */
+    public AbstractCard(String name, String id, final CardType type, final CardValue value){
         super(name,id);
+        this.cardType = type;
+        this.cardValue = value;
         this.name = name;
         faceUp = false;
+        count++;
     }
 
+    /**
+     * The top left corner
+     */
+    private final CardType cardType;
+    /**
+     * The middle
+     */
+    private final CardValue cardValue;
+
+    /**
+     * Getter for CardType
+     * @return Object that implements CardType
+     */
+    public CardType getCardType(){return this.cardType;}
+
+    /**
+     * Getter for CardValue
+     * @return Object that implements CardValue
+     */
+    public CardValue getCardValue(){return this.cardValue;}
 
     /**
      * Method to flip the card.
@@ -48,23 +88,45 @@ public abstract class AbstractCard extends GameObject{
 
     public final Image getBack(){return this.back;}
 
-    public abstract String toString();
+    //public abstract String toString();
 
     public abstract boolean equals(Object o);
 
     private static final int cardMiddleLength = 10;
 
-    static String cardRep(final String top, final String center){
-        final String topFormatted = top.substring(0,1);
+    /*
+    public static String cardRep(final String top, final String center) {
+        final int leftPadding = (cardMiddleLength - center.length()) / 2;
+        final int rightPadding = cardMiddleLength - center.length() - leftPadding;
+
         return "+" + "-".repeat(cardMiddleLength) + "+\n" +
-                "|" + topFormatted + " ".repeat(cardMiddleLength - 1) + "|\n" +
+                "|" + top + " ".repeat(cardMiddleLength - top.length()) + "|\n" +
                 "|" + " ".repeat(cardMiddleLength) + "|\n" +
                 "|" + " ".repeat(cardMiddleLength) + "|\n" +
-                "|" + " ".repeat(cardMiddleLength/2) + center +  " " .repeat(cardMiddleLength/2 - 1) + "|\n" +
+                "|" + " ".repeat(leftPadding) + center + " ".repeat(rightPadding) + "|\n" +
                 "|" + " ".repeat(cardMiddleLength) + "|\n" +
                 "|" + " ".repeat(cardMiddleLength) + "|\n" +
                 "+" + "-".repeat(cardMiddleLength) + "+";
     }
+     */
+
+    @Override
+    public String toString(){
+        final String top = this.cardType.getCardType().substring(0,1);
+        final String center = this.cardValue.getCardValue();
+        final int leftPadding = (cardMiddleLength - center.length()) / 2;
+        final int rightPadding = cardMiddleLength - center.length() - leftPadding;
+
+        return "+" + "-".repeat(cardMiddleLength) + "+\n" +
+                "|" + top + " ".repeat(cardMiddleLength - top.length()) + "|\n" +
+                "|" + " ".repeat(cardMiddleLength) + "|\n" +
+                "|" + " ".repeat(cardMiddleLength) + "|\n" +
+                "|" + " ".repeat(leftPadding) + center + " ".repeat(rightPadding) + "|\n" +
+                "|" + " ".repeat(cardMiddleLength) + "|\n" +
+                "|" + " ".repeat(cardMiddleLength) + "|\n" +
+                "+" + "-".repeat(cardMiddleLength) + "+";
+    }
+
 
 
 }
