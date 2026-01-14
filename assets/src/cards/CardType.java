fd16c6b8f2781785d6f79ecd157bc33911ad6a65
jtmilleer <jtmilleer05@gmail.com>
2026-01-14 14:42:04 -0600

    javadocs

--- assets/src/cards/CardType.java
+++ assets/src/cards/CardType.java
@@ -1,6 +1,19 @@
 package cards;
 
-public interface CardType {
+/**
+ * Interface that is used for polymorphism in <code>AbstractCard</code>.
+ * Each card has a type, i.e. a suit or a color.
+ * This interface is usually implemented by an <code>enum</code>
+ * @author Jordan Miller
+ */
+interface CardType {
+
+
+    //TODO both methods needed??
+    /**
+     * Method to get the card type represented by a <code>String</code>
+     * @return <code>String</code> containing information about the card type
+     */
     String getCardType();
 
     String toString();
--- assets/src/cards/CardValue.java
+++ assets/src/cards/CardValue.java
@@ -1,9 +1,25 @@
 package cards;
 
-public interface CardValue {
+/**
+ * Interface that is used for polymorphism in <code>AbstractCard</code>.
+ * Each card has a value, usually a numeric value, or the card does something special, i.e. uno reverse card.
+ * <p>The card will have an int value tied to the card, in the case of the numeric value, the int is the corresponding number.
+ * In the case of a non-numeric value, a negative integer is usually tied to it.
+ * This interface is usually implemented by an <code>enum</code>
+ * @author Jordan Miller
+ */
+interface CardValue {
+    /**
+     * Method to get the card value in a <code>String</code>
+     * @return <code>String</code> containing information about the card value
+     */
     String getCardValue();
 
     String toString();
 
+    /**
+     * Method to get the numerical value associated with each card value.
+     * @return <code>int</code> value of card.
+     */
     int getNumericValue();
 }
